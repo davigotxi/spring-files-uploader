30656f15593ea7d8b29bf958abc3b6609682d317
davigotxi <davigotxi@gmail.com>
2020-06-12 12:18:00 +0700

    first version

--- src/main/java/com/davigotxi/springfilesuploader/SpringFilesUploaderApplication.java
+++ src/main/java/com/davigotxi/springfilesuploader/SpringFilesUploaderApplication.java
@@ -1,13 +1,27 @@
 package com.davigotxi.springfilesuploader;
 
+import com.davigotxi.springfilesuploader.storage.StorageProperties;
+import com.davigotxi.springfilesuploader.storage.StorageService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringFilesUploaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFilesUploaderApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
