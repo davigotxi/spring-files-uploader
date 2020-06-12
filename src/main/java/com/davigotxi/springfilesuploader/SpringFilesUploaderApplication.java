6d229c41b0194186de4acbc4fba24b9c5aace80a
davigotxi <davigotxi@gmail.com>
2020-06-12 11:10:01 +0700

    Initial commit

---
+++ src/main/java/com/davigotxi/springfilesuploader/SpringFilesUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.davigotxi.springfilesuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilesUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilesUploaderApplication.class, args);
+	}
+
+}
